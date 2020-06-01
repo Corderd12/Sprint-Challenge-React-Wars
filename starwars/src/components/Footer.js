17cf4f9b2b13c18d6ffd8afe0006ffd802790d83
Your Name <you@example.com>
2020-06-01 14:17:39 -0400

    MVP met, working on stretch

---
+++ starwars/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Centeredh = styled.h5`
+    text-align: center;
+`;
+
+
+function Footer(){
+    return (
+        <div>
+            <Centeredh>Made in React - Dylan Corder</Centeredh>
+        </div>
+    )
+}
+
+export default Footer 
